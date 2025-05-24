cf3925a32ca326d54500883602cdc2fa6b28fd40
Gaurav Kumar <mr.gauravkumar8757@gmail.com>
2025-05-24 19:19:24 +0530

    updates

---
+++ src/routes/contact.jsx
@@ -0,0 +1,92 @@
+import { Form } from "react-router-dom";
+
+export default function Contact() {
+  const contact = {
+    first: "Gaurav",
+    last: "Kumar",
+    avatar: "https://avatars.githubusercontent.com/u/94515205?v=4&size=200x200",
+    github: "https://github.com/Gaurav8757",
+    notes: "Gaurav's Github Accounts",
+    favorite: true,
+  };
+
+  return (
+    <div id="contact">
+      <div>
+        <img
+          key={contact.avatar}
+          src={
+            contact.avatar ||
+            `https://robohash.org/${contact.id}.png?size=200x200`
+          }
+        />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.github && (
+          <p>
+            <a
+              target="_blank"
+              href={`https://github.com/${contact.github}`}
+            >
+              {contact.github}
+            </a>
+          </p>
+        )}
+
+        {contact.notes && <p>{contact.notes}</p>}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+          <Form
+            method="post"
+            action="destroy"
+            onSubmit={(event) => {
+              if (
+                !confirm(
+                  "Please confirm you want to delete this record."
+                )
+              ) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Favorite({ contact }) {
+  const favorite = contact.favorite;
+  return (
+    <Form method="post">
+      <button
+        name="favorite"
+        value={favorite ? "false" : "true"}
+        aria-label={
+          favorite
+            ? "Remove from favorites"
+            : "Add to favorites"
+        }
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </Form>
+  );
+}
