7a0d42851098a9600bc57f7eec03429317b85147
Gaurav Kumar <mr.gauravkumar8757@gmail.com>
2025-05-24 19:08:54 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
 import { RouterProvider } from 'react-router-dom'
 import router from './App.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}>
       <App />
+
     </RouterProvider>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+
+    />
   </StrictMode>,
 )
